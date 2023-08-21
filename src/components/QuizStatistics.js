57a8671fa12e323aef9371e055fb5b492f6cd387
RomaChumak <romachumak355@gmail.com>
2023-08-21 18:15:40 +0300

    d

--- src/components/QuizStatistics.js
+++ src/components/QuizStatistics.js
@@ -1,5 +1,5 @@
 import data from '../data.json'
-import { QuizStatList } from './QuizStatList'
+import {QuizStatList} from './QuizStatList'
 export const Statistics = () => {
     <section>
           <h2>Upload stats</h2>
